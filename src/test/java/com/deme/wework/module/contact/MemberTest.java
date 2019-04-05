82410ec8b0799171f357120eb77e35df43ff7d45
Gloria-HH <zhihuilvy@gmail.com>
2019-04-05 21:49:08 +0800

    add member create

--- src/test/java/com/deme/wework/module/contact/MemberTest.java
+++ src/test/java/com/deme/wework/module/contact/MemberTest.java
@@ -4,6 +4,8 @@ import com.deme.wework.utils.StringUtil;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import java.util.Arrays;
 import java.util.HashMap;
@@ -25,21 +27,29 @@ public class MemberTest {
                 .body("errcode", equalTo(0));
     }
 
-    @Test
-    public void create() {
+    @ParameterizedTest
+    @ValueSource(strings={"name_","test_","hello_"})
+    public void create(final String prefix) {
         HashMap<String, Object> hashMap = new HashMap<String, Object>() {{
             String random = StringUtil.random();
-            put("userid", "id" + random);
-            put("name", "name" + random);
+            String name=prefix+random;
+            put("userid", name);
+            put("name", name);
             put("alias", "alias" + random);
             put("department", Arrays.asList(1));
             put("is_leader_in_dept",Arrays.asList(1));
             put("email",random+"@sina.com");
-            put("mobile","13"+random.substring(0,9));
+            put("mobile","13"+random.substring(4));
 
         }};
 
         member.create(hashMap).then().statusCode(200)
                 .body("errcode", equalTo(0));
+    }
+
+    @Test
+    public void mobileTest(){
+        String random = StringUtil.random();
+        System.out.println(random.substring(4));
     }
 }
